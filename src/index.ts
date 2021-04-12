17d4022cc1190511be583d4c211bda76380a625e
Clinton Andrews <clint@pollyspeople.net>
2021-04-12 14:41:48 -0400

    remove unused types

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Comparer } from './dataSource/comparers';
-import { Operations, Logics, IOperationsObj } from './enums';
+import { Operations, Logics } from './enums';
 
 export type ChangeFQLHander<Tobj> = (fql: FilterQueryLanguage<Tobj>) => void;
-export type RemoveFilterHandler<Tobj> = (field: FitlerQueryField<Tobj>) => void;
-export type GetDefaultFilterQueryHandler<Tobj> = (field: FitlerQueryField<Tobj>) => FilterQuery<Tobj>;
+export type RemoveFilterHandler<Tobj> = (field: FilterQueryField<Tobj>) => void;
+export type GetDefaultFilterQueryHandler<Tobj> = (field: FilterQueryField<Tobj>) => FilterQuery<Tobj>;
 
 export type FilterUpdateHandler<Tobj> = (filterQuery: FilterQuery<Tobj>) => void;
 export type FilterItemValue<Tobj> = (Tobj[keyof Tobj] | unknown);
@@ -13,14 +13,14 @@ export interface FilterItem<Tobj> {
   value: FilterItemValue<Tobj>,
 }
 
-export type FitlerQueryField<Tobj> = (keyof Tobj) | string | (keyof Tobj)[] | string[];
+export type FilterQueryField<Tobj> = (keyof Tobj) | string | (keyof Tobj)[] | string[];
 
 export type FilterMapper<Tobj> = Partial<{ [Key: string]: Comparer<any> }> |
   Partial<{ [Key in keyof Tobj]: Comparer<any> }>;
 
 export interface FilterQuery<Tobj> {
   logic: Logics,
-  field: FitlerQueryField<Tobj>,
+  field: FilterQueryField<Tobj>,
   filterItems: FilterItem<Tobj>[],
 }
 
@@ -28,20 +28,3 @@ export interface FilterQueryLanguage<Tobj> {
   logic: Logics,
   filterQueries: FilterQuery<Tobj>[],
 }
-
-export interface IGetDefaultFilterQuery<Tobj> {
-  getDefaultFilterQuery?: GetDefaultFilterQueryHandler<Tobj>,
-}
-
-export interface IDefaultFilterProps {
-  getDefaultFilterQuery: () => void;
-}
-
-export type FilterProps<Tobj, Props = {}> = {
-  label: string;
-  field: FitlerQueryField<Tobj>,
-  labelClassName?: string,
-  filterQuery?: FilterQuery<Tobj>,
-  onFilterUpdate?: FilterUpdateHandler<Tobj>,
-  shown?: boolean,
-} & Props & IGetDefaultFilterQuery<Tobj>;
